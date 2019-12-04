58633cd28cbf6332af3fad8fe97a1fa0e33ff59c
mphelps1978 <phelps.michael@live.com>
2019-12-04 17:46:56 -0600

    MVP almost Done, trying to get the menu to toggle

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,15 +1,35 @@
 /* This is the data we will be using, study it but don't change anything, yet. */
 
-let menuItems = [
-  'Students',
-  'Faculty',
-  "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
-];
+let menuItems = ['Students', 'Faculty', "What's New", 'Tech Trends', 'Music', 'Log Out'];
 
-/* 
+function createMenu(array) {
+	const menu = document.createElement('div');
+	const list = document.createElement('ul');
+
+	menu.classList.add('menu');
+
+	menu.appendChild(list);
+
+	menuItems.forEach(item => {
+		const listItem = document.createElement('li');
+		listItem.textContent = item;
+		list.append(listItem);
+	});
+
+  const button = document.querySelector;(.menu-button);
+button.addEventListener('click'  =>{
+menu.classList.toggle('menu--open');
+
+})
+
+
+	return menu;
+}
+
+const siteHeader = document.querySelector('.menu-button');
+siteHeader.prepend(createMenu(menuItems));
+
+/*
 
   Step 1: Write a function that will create a menu component as seen below:
 
@@ -21,7 +41,7 @@ let menuItems = [
 
   The function takes an array as its only argument.
 
-  Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
+  Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array.
   Add those items to the <ul>
 
   Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
@@ -31,5 +51,5 @@ let menuItems = [
   Step 5: return the menu component.
 
   Step 6: add the menu component to the DOM.
-  
+
 */
